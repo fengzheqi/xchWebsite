843eabebce9a1403c7ed975aac62e7a1181101a4
tommyzqfeng <fengzheqiyx@163.com>
2017-03-06 22:15:54 +0800

    完成发送邮件功能

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/sendMail',function(req, res, next){
+router.post('/sendMail',function(req, res, next){
   // create reusable transporter object using the default SMTP transport
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'fengzheqiyx@gmail.com',
-      pass: 'gmail@2045635683'
+      user: 'your-email@XX.com',
+      pass: 'yourEmailPassword'
     }
   });
+  var mailContent = req.body.name + '<b>' + req.body.comments + '</b>';
   // setup email data with unicode symbols
   var mailOptions = {
     from: '"Tommyzqfeng" <fengzheqiyx@gmail.com>', // sender address
-    to: '811756202@qq.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world ?', // plain text body
-    html: '<b>Hello world ?</b>' // html body
+    to: req.body.email, // list of receivers
+    subject: '你的个人网站有新的留言', // Subject line
+    text: req.body.comments, // plain text body
+    html: mailContent // html body
   };
+  console.log(mailOptions);
   // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
@@ -32,6 +34,7 @@ router.get('/sendMail',function(req, res, next){
     }
     console.log('Message %s sent: %s', info.messageId, info.response);
   });
+  res.redirect('/');
 });
 
 module.exports = router;
